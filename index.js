f93191c2ff868c426f82078cd300aa119b86c001
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-07-27 21:40:16 -0700

    added enumerative comments

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const one = (prop, test, cases) => cases.reduce((truth, present) => truth ? true
 const all = (prop, test, cases) => cases.reduce((truth, present) => truth ? test(present, prop) : false, true);
 
 const none= (prop, test, cases) => cases.reduce((truth, present) => truth ? false : test(present, prop), false);
+
+/*
+                                                                    truth ? true : test(present, prop), true
+                                                                        always true - not a test
+                                                                    truth ? test(present, prop) : false, false
+                                                                        always false- not a test
+                                                                    truth ? test(present, prop) : true, true
+                                                                        doesn't preserve truth value - not useful
+                                                                    truth ? test(present, prop) : true, false
+                                                                        if ever false immediatly turns to true - not useful
+                                                                    truth ? false : test(present, prop), true
+                                                                        skips the first test - not exhastive
+*/
